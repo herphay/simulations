91e3f65046ccccae17535b4bf6c34fc7e82022b9
herphay <herphay@gmail.com>
2025-07-06 16:50:41 +0800

    population.py: started on interpolate_tfr func which will provide an interpolated asfr from the 5-yr grouped raw data
     - Uses pchip to get the curve shape
     - plots the final curve for visual inspection

--- population.py
+++ population.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 
 @dataclass
 class TFR:
+    std_ages = np.array([15, 22, 27, 32, 37, 42, 49])
     # Singapore 2024 ASFR data
     asfr_grp_2024 = np.array([2.3, 9.8, 42.6, 79.3, 50, 10.2, 0.7])
     start_age_2024 = np.arange(15, 50, 5)
@@ -29,5 +30,23 @@ def create_asfr_df(
         plt.show()
     
     return asfr_df
+
+
+def interpolate_tfr(
+        asfr_df: pd.DataFrame,
+        plot: bool = True
+    ) -> pd.DataFrame:
+    # Get curve shape of the asfr distribution
+    curve_shape = inter.pchip_interpolate(TFR.std_ages, # x axis -> standard ages with end and mid pts
+                                          asfr_df.loc[TFR.std_ages, 'asfr'], # corresponding asfr
+                                          asfr_df.index) # interpolate to all ages
+    
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(asfr_df.index, curve_shape)
+
+        plt.show()
+
+    return curve_shape
 
 main()
