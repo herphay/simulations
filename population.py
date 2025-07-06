d10cd35b3902c177a49cc036ef86be64c775abb1
herphay <herphay@gmail.com>
2025-07-06 15:48:18 +0800

    population.py: Added asfr df creation func
     - The func creates a asfr df with age as index and a 'asfr' col with constant asfr for each age in the group
     - Contains multiple hard coded values from assumption that asfr will always be for within 15-49 incl. range

--- population.py
+++ population.py
@@ -1,17 +1,33 @@
 import pandas as pd
 import numpy as np
-from scipy.interpolate import CubicSpline
+import scipy.interpolate as inter
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
 @dataclass
 class TFR:
-    group_asfr: np.ndarray = np.array([2.3, 9.8, 42.6, 79.3, 50, 10.2, 0.7])
-    group_mean_age: np.ndarray = np.arange(17, 50, 5)
+    # Singapore 2024 ASFR data
+    asfr_grp_2024 = np.array([2.3, 9.8, 42.6, 79.3, 50, 10.2, 0.7])
+    start_age_2024 = np.arange(15, 50, 5)
 
 def main() -> None:
     ...
 
 
+def create_asfr_df(
+        asfr_grp: np.ndarray,
+        start_age: np.ndarray,
+        plot: bool = True
+    ) -> pd.DataFrame:
+    asfr_df = pd.DataFrame({'asfr': [np.nan] * 35}, index=np.arange(15, 50))
+    asfr_df['asfr'] = pd.DataFrame({'asfr': asfr_grp}, index=start_age)['asfr']
+    asfr_df = asfr_df.ffill()
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(asfr_df.index, asfr_df.asfr)
+        plt.show()
+    
+    return asfr_df
 
 main()
