ececa6edf5b32040498ea2db21d7899cd9f054dc
herphay <herphay@gmail.com>
2025-07-19 18:46:27 +0800

    MIT 18.05: added dice_roller func to check if a specific number gets rolled

--- mit_stats_intro.py
+++ mit_stats_intro.py
@@ -31,6 +31,26 @@ def sample(
     return rng.choice(x, k, replace=replace)
 
 
+def dice_roller(
+        sides: int = 6,
+        times: int = 4,
+        repeat: int = 1000,
+        check_for: int = 6,
+        reset_rng_for_repeats: bool = False
+    ) -> float:
+    """
+    Roll a dice n times, check if a specific number turn up
+    """
+    if not reset_rng_for_repeats:
+        rng = np.random.default_rng()
+        rolls = rng.integers(1, sides + 1, (times, repeat))
+
+        prob_get = ((rolls == check_for).sum(axis=0) > 0).sum() / repeat
+        prob_theo = 1 - (5 / 6) ** times
+    
+    print(f'Experiment result of getting at least 1 {check_for} out of {times} dice rolls is: ' + 
+          f'{prob_get}, \nthe theoretical probability is {prob_theo:.3f}.')
+
 
 #%%
 if __name__ == '__main__':
