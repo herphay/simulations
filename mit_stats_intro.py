6e3d87d8d79d28944942c1cf7554f79f07e17717
herphay <herphay@gmail.com>
2025-07-19 18:22:35 +0800

    MIT 18.05: Added R sample equivalent: np random choice

--- mit_stats_intro.py
+++ mit_stats_intro.py
@@ -12,7 +12,23 @@ def main() -> None:
 
 #%%
 # Week 1
-
+def sample(
+        x: np.ndarray = np.arange(10),
+        k: int = 5,
+        replace: bool = False
+    ) -> np.ndarray:
+    """
+    sample(x,k) generates a random permutation of k objects from the vector x. 
+    That is, all k choices are different
+    """
+    # timeit('rng.integers(1, 7)', 'import numpy as np; rng = np.random.default_rng(); die = np.arange(1,7)', number=100000)
+    # performance: 0.0852
+    # timeit('rng.choice(die)', 'import numpy as np; rng = np.random.default_rng(); die = np.arange(1,7)', number=100000)
+    # performance: 0.3648
+    # 
+    # Better to use random integers for interger dice rolling, unless absolute permutation is required
+    rng = np.random.default_rng()
+    return rng.choice(x, k, replace=replace)
 
 
 
