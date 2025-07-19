d7805a8794b10e508be62f164b7069d9b258ea3c
herphay <herphay@gmail.com>
2025-07-19 18:19:11 +0800

    Added mit_stats_intro.py for MIT OpenCourseWare Intro to Stats & Prob class (18.05, 2022)
     - Will contain all R codes implemented in python
     - For my own learning

---
+++ mit_stats_intro.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main() -> None:
+    """
+    This file is for MIT OpenCourseWare - Introduction to Probability and Statistics
+    18.05 | Spring 2022 | Undergraduate
+
+    Course is conducted using R -> I will translate it into python
+    """
+
+#%%
+# Week 1
+
+
+
+
+#%%
+if __name__ == '__main__':
+    main()
