910953007a49249de7a2516bf851cb9e310e84d9
herphay <herphay@gmail.com>
2025-08-05 21:05:57 +0800

    Create and initialize Rock Paper Scissor Evolution Simulator under rps.py

---
+++ rps.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import math
+
+from collections.abc import Iterable
+
+def main():
+    ...
+
+
+def rps_winner(
+        party1: str | Iterable[str] = 'r',
+        party2: str | Iterable[str] = 'r',
+    ) -> bool | Iterable[bool]:
+    """
+    Determine the winner of a rock paper scissor game
+    """
+
+
+if __name__ == '__main__':
+    main()
