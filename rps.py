aa6dcfd745617d4a27ed48f041360063d269ac87
herphay <herphay@gmail.com>
2025-08-06 06:44:31 +0800

    Added conversion to int for rps and added win/lose condition

--- rps.py
+++ rps.py
@@ -14,12 +14,30 @@ def rps_winner(
     ) -> bool | Iterable[bool]:
     """
     Determine the winner of a rock paper scissor game:
-        r > s | s > p | p > r
+        r > s  | s > p  | p > r
+    
+        r: 114 | p: 112 | s: 115
+
+        rr: 0  | pp: 0  | ss: 0    # Tie
+        rs: -1 | sp: 3  | pr: -2   # 1st position win
+        sr: 1  | ps: -3 | rp: 2    # 2nd position win
     """
-    if len(party1) == len(party2):
-        ...
-    else:
+    if len(party1) != len(party2):
         raise ValueError('The length of both inputs must match')
+    
+    for arr in [party1, party2]:
+        # Convert input into np array of single byte chars
+        arr = np.array(arr, dtype='S1')
+        # Convert the single byte chars into ASCII ints
+        arr = arr.view(np.uint8)
+        
+        # Check whether input is a subset of proper inputs
+        if not set([112, 114, 115]) >= set(arr):
+            raise ValueError('Unacceptable input: only rps accepted')
+    
+    return party1, party2
+    
+        
 
 
 if __name__ == '__main__':
