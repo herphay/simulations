ebf3715fd41c58d230135e2364a269f622f5f4c7
herphay <herphay@gmail.com>
2025-08-07 06:40:19 +0800

    Minor updates to rps_winner
     - repositioned value < 0 to be all party1 winning, vise versa
     - Updated values to be int8 instead of uint8 -> allows -ve values
     - added test initialization

--- rps.py
+++ rps.py
@@ -19,9 +19,10 @@ def rps_winner(
         r: 114 | p: 112 | s: 115
 
         rr: 0  | pp: 0  | ss: 0    # Tie
-        rs: -1 | sp: 3  | pr: -2   # 1st position win
-        sr: 1  | ps: -3 | rp: 2    # 2nd position win
+        rs: -1 | ps: -3 | pr: -2   # 1st position win
+        sr: 1  | sp: 3  | rp: 2    # 2nd position win
     """
+    # from rps import rps_winner;a=list('rpsrppssr');b=list('rpsssrrpp')
     if len(party1) != len(party2):
         raise ValueError('The length of both inputs must match')
     
@@ -29,7 +30,7 @@ def rps_winner(
         # Convert input into np array of single byte chars
         arr = np.array(arr, dtype='S1')
         # Convert the single byte chars into ASCII ints
-        arr = arr.view(np.uint8)
+        arr = arr.view(np.int8)
         
         # Check whether input is a subset of proper inputs
         if not set([112, 114, 115]) >= set(arr):
